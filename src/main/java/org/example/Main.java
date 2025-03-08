fefae43cfb49baa52aa854140fe5632790a50b52
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-07 20:28:44 -0500

    Proving repositorie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Storyu nof my life-Bon Jovi");
+        System.out.println("Story of my life-Bon Jovi");
+        for(int i = 1; i<=5; i++){
+            System.out.println("Hola "+i);
+        }
     }
 }
