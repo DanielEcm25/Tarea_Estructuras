eada5dc8207935c4442ef61d7bb4441ace5647ed
Juanc4str1ll0n <juancastrillon868@gmail.com>
2025-03-14 21:38:07 -0500

    Creacion de 3 Stacks que representan las torres y de scanner, insercion de discos a torre 1 mediante for

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,23 @@
 package org.example;
+
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
+
+    private static Stack<Integer> torre1 = new Stack<>();
+    private static Stack<Integer> torre2 = new Stack<>();
+    private static Stack<Integer> torre3 = new Stack<>();
+
     public static void main(String[] args) {
-        System.out.println("Story of my life-Bon Jovi");
-        for(int i = 1; i<=5; i++){
-            System.out.println("Hola "+i);
+        //Preguntándole al usuario con cuántos discos desea jugar
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("¿Con cuántos discos desea jugar?");
+        int discos = entrada.nextInt();
+        //Insertando los discos a la torre 1
+        for (int i = discos; i >= 1; i--) {
+            torre1.push(i);
         }
+
     }
 }
